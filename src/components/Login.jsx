6537aefe7cca69d6a9c62b3923a2168ee97aa3c0
BasilInk <vasiltsov_s_a@mail.ru>
2022-05-12 13:27:11 +0300

    version1.0

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,22 +2,23 @@ import { Box, Button, Container, Grid } from "@mui/material";
 import { useContext } from "react";
 import { Context } from "../index";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-// import { async } from "@firebase/util";
-
 
 const Login = () => {
-
-  const {auth} = useContext(Context)
+  const { auth } = useContext(Context);
 
   const login = async () => {
     const provider = new GoogleAuthProvider();
-    const {user} = await signInWithPopup(auth, provider);
-    console.log(user)
+    const { user } = await signInWithPopup(auth, provider);
+    console.log(user);
   };
 
   return (
     <Container>
-      <Grid container style={{height: window.innerHeight - 50}} className="loginContainer">
+      <Grid
+        container
+        style={{ height: window.innerHeight - 50 }}
+        className="loginContainer"
+      >
         <Grid>
           <Box p={5}>
             <Button
@@ -26,7 +27,7 @@ const Login = () => {
                 backgroundColor: "inherit",
                 color: "#4F4D8C",
               }}
-              onClick = {login}
+              onClick={login}
             >
               login with a google account
             </Button>
